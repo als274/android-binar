3ee38d0dffa70ec3c9fcea2bfed19489c85c9dd7
muwqi <muqoffikhos@gmail.com>
2023-06-07 14:27:15 +0700

    commit pertama

---
+++ Test Suites/Add Product.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Add Product</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4f969c50-e70f-4fa2-8d93-3705f78ccfad</testSuiteGuid>
+   <testCaseLink>
+      <guid>648a57e7-cda1-409d-b4eb-3f0b00d7e8d3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Add Product/AP01 - User want to add product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
